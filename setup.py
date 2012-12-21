ae243ffd070791d8da4c7977942197ce80c2b3cd
dimka665 <dimka665@gmail.com>
2012-12-21 05:44:21 +0400

    Update distribution

--- setup.py
+++ setup.py
@@ -3,15 +3,17 @@ from distutils.core import setup
 long_description = open('README.rst').read()
 
 setup(
-    name='django-markupfield',
-    version="1.1.0-dev",
-    package_dir={'markupfield': 'markupfield'},
-    packages=['markupfield', 'markupfield.tests'],
+    name='django-markitup-field',
+    version="1.2-dev",
+    package_dir={'markitup_field': 'markitup_field'},
+#    packages=['markitup_field', 'markitup_field.tests'],
+    packages=['markitup_field'],
+    py_modules=['markitup_field'],
     description='Custom Django field for easy use of markup in text fields',
     author='James Turk',
     author_email='james.p.turk@gmail.com',
     license='BSD License',
-    url='http://github.com/jamesturk/django-markupfield/',
+    url='http://github.com/dimka665/django-markitup-field/',
     long_description=long_description,
     platforms=["any"],
     classifiers=[
