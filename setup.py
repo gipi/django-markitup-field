86787903df9d1ecb47638cfab5cbf86617d9c5cf
dimka665 <dimka665@gmail.com>
2012-12-29 23:21:44 +0400

    setup.py fixed

--- setup.py
+++ setup.py
@@ -1,20 +1,35 @@
 from distutils.core import setup
+import os
 
 long_description = open('README.rst').read()
 
+def _static_files(prefix):
+    return [os.path.join(prefix, pattern) for pattern in [
+        'markitup/*.*',
+        'markitup/sets/*.*',
+        'markitup/sets/*/*.*',
+        'markitup/sets/*/images/*.png',
+        'markitup/skins/*/*.*',
+        'markitup/skins/*/images/*.png',
+        'markitup/templates/*.*'
+    ]]
+
 setup(
     name='django-markitup-field',
-    version="1.2.2-dev",
-    package_dir={'markitup_field': 'markitup_field'},
-#    packages=['markitup_field', 'markitup_field.tests'],
-    packages=['markitup_field'],
-    py_modules=['markitup_field'],
+    version="1.2.10",
     description='Custom Django field for easy use of markup in text fields',
-    author='James Turk',
-    author_email='james.p.turk@gmail.com',
+    long_description=long_description,
+    author='Dmitry Voronin',
+    author_email='dimka665@gmail.com',
     license='BSD License',
     url='http://github.com/dimka665/django-markitup-field/',
-    long_description=long_description,
+    packages=['markitup_field',
+              'markitup_field.management', 'markitup_field.management.commands',
+              'markitup_field.templatetags',
+              'markitup_field.tests',
+              ],
+    package_data={'markitup_field': ['templates/markitup/*.html'] + _static_files('static')},
+
     platforms=["any"],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
@@ -24,5 +39,7 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Environment :: Web Environment',
-    ],
+        'Framework :: Django',
+        ],
 )
+
